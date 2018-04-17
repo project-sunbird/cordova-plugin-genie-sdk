0d4a73bac7326439e5db6e3c72a6b2dd58566968
Anil Gupta <anil.gupta@tarento.com>
2018-04-17 18:16:53 +0530

    Get boards from framework.

--- src/android/org/genie/FrameworkHandler.java
+++ src/android/org/genie/FrameworkHandler.java
@@ -13,12 +13,20 @@ import org.json.JSONException;
 public class FrameworkHandler {
 
     private static final String TYPE_GET_FRAMEWORK_DETAILS = "getFrameworkDetails";
+    private static final String TYPE_GET_BOARDS = "getBoards";
+    private static final String CATEGORY_BOARD = "board";
+
+    private static Framework framework = null;
 
     public static void handle(JSONArray args, final CallbackContext callbackContext) {
         try {
             String type = args.getString(0);
             if (type.equals(TYPE_GET_FRAMEWORK_DETAILS)) {
                 getFrameworkDetails(callbackContext);
+            } else {
+                if (type.equals(TYPE_GET_BOARDS)) {
+                    getBoards(callbackContext);
+                }
             }
 
         } catch (JSONException e) {
@@ -26,13 +34,44 @@ public class FrameworkHandler {
         }
     }
 
-    private static void getFrameworkDetails(final CallbackContext callbackContext) throws JSONException {
+    private static void getBoards(CallbackContext callbackContext) {
+        if (framework == null) {
+            FrameworkDetailsRequest.Builder builder = new FrameworkDetailsRequest.Builder();
+            builder.defaultFrameworkDetails();
+            GenieResponse<Framework> genieResponse = GenieService.getService().getFrameworkService().getFrameworkDetails(builder.build());
+            if (!genieResponse.getStatus()) {
+                callbackContext.error(GsonUtil.toJson(genieResponse));
+                return;
+            }
+
+            framework = genieResponse.getResult();
+        }
+
+        List<String> boards = new ArrayList<>();
+        Map<String, Object> frameworkMap = GsonUtil.fromJson(framework.getFramework(), Map.class);
+        if (frameworkMap.containsKey("categories")) {
+            List<Map> categories = (List<Map>) frameworkMap.get("categories");
+            for (Map category : categories) {
+                if (CATEGORY_BOARD.equals(category.get("code"))) {
+                    List<Map> terms = (List<Map>) category.get("terms");
+                    for (Map term : terms) {
+                        boards.add((String) term.get("name"));
+                    }
+                }
+            }
+        }
+
+        callbackContext.success(GsonUtil.toJson(boards));
+    }
+
+    private static void getFrameworkDetails(final CallbackContext callbackContext) {
         FrameworkDetailsRequest.Builder frameworkDetailsRequest = new FrameworkDetailsRequest.Builder();
         frameworkDetailsRequest.defaultFrameworkDetails();
 
         GenieService.getAsyncService().getFrameworkService().getFrameworkDetails(frameworkDetailsRequest.build(), new IResponseHandler<Framework>() {
             @Override
             public void onSuccess(GenieResponse<Framework> genieResponse) {
+                framework = genieResponse.getResult();
                 callbackContext.success(GsonUtil.toJson(genieResponse));
             }
 
--- www/framework.js
+++ www/framework.js
@@ -5,6 +5,10 @@ var PLUGIN_NAME = 'GenieSDK';
 var framework = {
     getFrameworkDetails: function(requestJson, success, error) {
         exec(success, error, PLUGIN_NAME, this.action(), ["getFrameworkDetails", requestJson]);
+    },
+
+    getBoards: function(requestJson, success, error) {
+        exec(success, error, PLUGIN_NAME, this.action(), ["getBoards", requestJson]);
     }
 };
 
