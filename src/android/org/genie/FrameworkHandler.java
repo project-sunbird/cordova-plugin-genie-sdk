45df20e8d86575bb6467c9d950d5099c9557a57b
Anil Gupta <ani2824@gmail.com>
2018-08-22 16:17:49 +0530

    Issue #SB-6123 feat: Added getChannelDetails API

--- src/android/org/genie/FrameworkHandler.java
+++ src/android/org/genie/FrameworkHandler.java
@@ -3,6 +3,8 @@ package org.genie;
 import org.apache.cordova.CallbackContext;
 import org.ekstep.genieservices.GenieService;
 import org.ekstep.genieservices.commons.IResponseHandler;
+import org.ekstep.genieservices.commons.bean.Channel;
+import org.ekstep.genieservices.commons.bean.ChannelDetailsRequest;
 import org.ekstep.genieservices.commons.bean.Framework;
 import org.ekstep.genieservices.commons.bean.FrameworkDetailsRequest;
 import org.ekstep.genieservices.commons.bean.GenieResponse;
@@ -12,6 +14,7 @@ import org.json.JSONException;
 
 public class FrameworkHandler {
 
+    private static final String TYPE_GET_CHANNEL_DETAILS = "getChannelDetails";
     private static final String TYPE_GET_FRAMEWORK_DETAILS = "getFrameworkDetails";
     private static final String TYPE_PERSIST_FRAMEWORK_DETAILS = "persistFrameworkDetails";
 
@@ -19,7 +22,9 @@ public class FrameworkHandler {
         try {
             String type = args.getString(0);
 
-            if (type.equals(TYPE_GET_FRAMEWORK_DETAILS)) {
+            if (type.equals(TYPE_GET_CHANNEL_DETAILS)) {
+                getChannelDetails(args, callbackContext);
+            } else if (type.equals(TYPE_GET_FRAMEWORK_DETAILS)) {
                 getFrameworkDetails(args, callbackContext);
             } else if (TYPE_PERSIST_FRAMEWORK_DETAILS.equals(type)) {
                 persistFrameworkDetails(args, callbackContext);
@@ -28,6 +33,23 @@ public class FrameworkHandler {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void getChannelDetails(JSONArray args, CallbackContext callbackContext) throws JSONException {
+        final String requestJson = args.getString(1);
+
+        ChannelDetailsRequest.Builder builder = GsonUtil.fromJson(requestJson, ChannelDetailsRequest.Builder.class);
+        GenieService.getAsyncService().getFrameworkService().getChannelDetails(builder.build(), new IResponseHandler<Channel>() {
+            @Override
+            public void onSuccess(GenieResponse<Channel> genieResponse) {
+                callbackContext.success(GsonUtil.toJson(genieResponse));
+            }
+
+            @Override
+            public void onError(GenieResponse<Channel> genieResponse) {
+                callbackContext.error(GsonUtil.toJson(genieResponse));
+            }
+        });
     }
 
     private static void getFrameworkDetails(JSONArray args, final CallbackContext callbackContext) throws JSONException {
--- www/framework.js
+++ www/framework.js
@@ -3,6 +3,10 @@ var exec = require("cordova/exec");
 var PLUGIN_NAME = 'GenieSDK';
 
 var framework = {
+    getChannelDetails: function(requestJson, success, error) {
+        exec(success, error, PLUGIN_NAME, this.action(), ["getChannelDetails", requestJson]);
+    },
+
     getFrameworkDetails: function(requestJson, success, error) {
         exec(success, error, PLUGIN_NAME, this.action(), ["getFrameworkDetails", requestJson]);
     },
